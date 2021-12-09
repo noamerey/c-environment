57bd3d46f31394545314c749ca74fa2f49aca35d
Noa Merey <noa.merey@edu.devinci.fr>
2021-12-09 16:47:03 +0100

    ekip

---
+++ main.c
@@ -0,0 +1,18 @@
+int main(){
+        int ekip = 0;
+        while(ekip <=10){
+                printf("%d\n",ekip);
+                ekip = ekip + 1;
+                }
+        return 0;
+
+}
+
+
+
+
+
+
+
+
+
